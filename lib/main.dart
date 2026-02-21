b2f58ff7418b0eccd1f17f97853cb3fff516e828
ABDELILAH AHARCHA <125594279+ABDOU-AHARCHA@users.noreply.github.com>
2026-02-21 22:14:01 +0000

    implementing the routing in the app (adding necassary files and packages)

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:notes_app/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'models/note.dart';
 import 'models/note_category.dart';
 import 'services/hive_category_service.dart';
-import 'screens/home_screen.dart';
 import 'theme/app_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'screens/signup_screen.dart';
-import 'screens/auth_gate.dart';
+import 'router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  print('🚀 APP - Starting Firebase initialization');
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  print('✅ APP - Firebase initialized');
 
-  // Initialize Hive
   await Hive.initFlutter();
-  print('✅ APP - Hive initialized');
-
-  // Register adapters
   Hive.registerAdapter(NoteAdapter());
   Hive.registerAdapter(NoteCategoryAdapter());
-
-  // Open boxes
   await Hive.openBox<Note>('notes');
   await Hive.openBox<NoteCategory>('categories');
-  print('✅ APP - Hive boxes opened');
 
-  // Initialize default categories
   final categoryService = CategoryService();
   await categoryService.initializeDefaultCategories();
-  print('✅ APP - Categories initialized');
 
   runApp(const NotesApp());
 }
@@ -43,12 +30,11 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('🏗️ APP - Building NotesApp');
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Notes App',
       theme: AppTheme.lightTheme,
-      home: const SplashScreen(),
+      routerConfig: router,
     );
   }
 }
